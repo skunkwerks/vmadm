793e6c115799eef04e385e0bdb67359c8f118e9a
Heinz N. Gies <heinz@licenser.net>
2017-09-25 13:16:38 +0200

    Fix autostart->autoboot and add autoboot feature

--- src/main.rs
+++ src/main.rs
@@ -209,7 +209,18 @@ fn run() -> i32 {
     }
 }
 
-fn startup(_conf: &Config) -> Result<i32, Box<Error>> {
+fn startup(conf: &Config) -> Result<i32, Box<Error>> {
+    let db = JDB::open(conf)?;
+    for e in db.iter() {
+        let jail = db.get(&e.uuid)?;
+        if jail.config.autoboot && jail.outer.is_none() {
+            println!("Starting jail {}", jail.idx.uuid);
+            jail.start(conf)?;
+        } else {
+            println!("Skipping startup for jail {}", jail.idx.uuid);
+
+        }
+    };
     Ok(0)
 }
 
